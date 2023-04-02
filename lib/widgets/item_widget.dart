74d93b5cf03023ddbee99dd667097f308d1ee082
VANSH TRIVEDI <vanshtrivedi12345@gmail.com>
2023-04-02 22:11:59 +0530

    Before state management

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:catalogapp/models/catalog.dart';
 
-
-
 class ItemWidget extends StatelessWidget {
   final Item item;
 
   const ItemWidget({super.key, required this.item});
-
- 
 
   @override
   Widget build(BuildContext context) {
